9bd7f14768ad29f92db2aa357a3c3f542d4dff1c
Giuliana Bezerra <giu.drawer@gmail.com>
2026-01-17 14:33:29 -0300

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,13 +1,1 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from app.rag import ask_rag
-
-app = FastAPI()
-
-class Question(BaseModel):
-    question: str
-
-@app.post("/pergunta")
-def perguntar(data: Question):
-    answer = ask_rag(data.question)
-    return {"answer": answer}
+# TODO Implementar
